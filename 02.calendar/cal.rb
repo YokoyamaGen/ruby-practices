1a61a0011aaedee108d43c44c2d35f350666e532
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-05-21 09:40:28 +0900

    カレンダーのプログラム(ruby)に関するレビューでの指摘事項を反映
    カレンダーの月に関する情報の設定処理を三項演算子に修正
    ループ処理の中で実行していた月初の表示位置の調整処理をループ処理の前に移動
    日付を表示する処理をDRYに修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -12,11 +12,7 @@ opt.parse!(ARGV, into: params)
 
 if params[:y].nil?
   year = Date.today.year
-  month = (if params[:m].nil?
-             Date.today.month
-           else
-             params[:m].to_i
-           end)
+  month = params[:m].nil? ? Date.today.month : params[:m].to_i
 else
   year = params[:y].to_i
   month = params[:m].to_i
@@ -28,18 +24,10 @@ month_end = Date.new(year, month, -1)
 puts "#{month}月 #{year}".center(21) 
 puts " 日 月 火 水 木 金 土"
 
-add_week_number = 1
 default_output_width = 3
+printf("%#{month_beginning.wday * default_output_width}s", "")
 
 (month_beginning..month_end).each do |calendar_day|
-  output_format = (if calendar_day == month_beginning
-                     (calendar_day.wday + add_week_number) * default_output_width
-                   else
-                     default_output_width
-                   end)
-  if calendar_day.saturday? || calendar_day == month_end
-    printf("%#{output_format}d\n", calendar_day.day)
-  else
-    printf("%#{output_format}d", calendar_day.day)
-  end
+  new_line = calendar_day.saturday? || calendar_day == month_end ? "\n" : ""
+  printf("%#{default_output_width}d#{new_line}", calendar_day.day)
 end
