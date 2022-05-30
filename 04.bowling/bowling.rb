0c1aec1828a97be628b5956ce4644ba55bdb1580
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-05-30 12:12:11 +0900

    ボウリングのスコア計算プログラムの指摘事項を反映
    変数定義の定義場所を変更。変数を使用するロジックの近くに定義。
    定数定義の定義場所を変更。プログラムの上部に定義。
    分岐を三項演算子に修正。
    一度しか使用していない変数を削除。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,23 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
-
-frames_tmp = []
-shots = []
+scores = ARGV[0].split(',')
 
 NINE_FRAME = 9
 BEFORE_LAST_THROW = scores.count - 1
 STRIKE = 10
 FIRST_THROW = 1
+SPARE = 10
 
+shots = []
+frames_tmp = []
 scores.each_with_index do |s, i|
-  shots << if s == 'X'
-             STRIKE
-           else
-             s.to_i
-           end
+  shots << (s == 'X' ? STRIKE : s.to_i)
 
   next if (shots[0] != STRIKE && shots.count == FIRST_THROW) || (frames_tmp.count >= NINE_FRAME && i < BEFORE_LAST_THROW)
 
@@ -29,7 +24,6 @@ frames = frames_tmp.each_cons(3).map(&:flatten)
 frames += frames_tmp[-2..].each_cons(2).map(&:flatten)
 frames << frames_tmp[-1]
 
-SPARE = 10
 point = 0
 frames.each do |frame|
   point += if frame[0] == STRIKE || frame[0..1].sum == SPARE
